c36af895dd32b8bb42ed00c9499710da05f21964
Fedechkin Aleksei <alex@centralhardware.me>
2024-11-08 08:17:33 +0100

    bump clickhouse

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,14 @@ repositories {
     mavenCentral()
 }
 
+val clickhouseVersion = "0.7.1-patch1"
+
 dependencies {
     implementation("dev.inmo:tgbotapi:20.0.0")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("com.clickhouse:clickhouse-jdbc:0.7.1")
-    implementation("com.clickhouse:clickhouse-http-client:0.7.1")
+    implementation("com.clickhouse:clickhouse-jdbc:$clickhouseVersion")
+    implementation("com.clickhouse:clickhouse-http-client:$clickhouseVersion")
     implementation("org.lz4:lz4-java:1.8.0")
     implementation("com.github.seratch:kotliquery:1.9.0")
 }
